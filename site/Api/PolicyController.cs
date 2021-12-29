5fce3a3bf851c3cf0cbeb0a803ae33a28920e621
Warwick Stone <warwick_stone@fastmail.com>
2021-12-29 17:37:25 +1100

    now using data returned from api

--- site/Api/PolicyController.cs
+++ site/Api/PolicyController.cs
@@ -21,7 +21,7 @@ namespace site.Api
             if (policy == null)
                 return NotFound();
             
-            return Ok(policy);
+            return Ok(new [] {1, 2, 3, 4, 5, 6});
         }
     }
 }
